b5e075ba4a670f96b1b24d6db5fbb012cc2e9e34
scorpion00100 <danbetheliryivuze@gmail.com>
2024-01-24 14:04:42 +0100

    update betty mistakes

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -6,18 +6,18 @@
  */
 static void print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 /**
@@ -28,13 +28,13 @@ static void print_grid(int grid[3][3])
  */
 int is_stable(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++)
-            if (grid[i][j] > 3)
-                return (0);
-    return (1);
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			if (grid[i][j] > 3)
+				return (0);
+	return (1);
 }
 
 /**
@@ -48,12 +48,12 @@ int is_stable(int grid[3][3])
  */
 int is_part_stable(int grid_copy[3][3], int grid[3][3], int i, int j)
 {
-    if (grid_copy[i][j] < 4)
-    {
-        grid[i][j] += 1;
-        return (1);
-    }
-    return (0);
+	if (grid_copy[i][j] < 4)
+	{
+		grid[i][j] += 1;
+		return (1);
+	}
+	return (0);
 }
 
 /**
@@ -67,25 +67,25 @@ int is_part_stable(int grid_copy[3][3], int grid[3][3], int i, int j)
  */
 int topple_sandpile(int grid1[3][3], int grid_copy[3][3], int i, int j)
 {
-    int topple = 0;
+	int topple = 0;
 
-    if (i - 1 >= 0)
-        topple += is_part_stable(grid_copy, grid1, i - 1, j);
-    else
-        topple += 1;
-    if (i + 1 < 3)
-        topple += is_part_stable(grid_copy, grid1, i + 1, j);
-    else
-        topple += 1;
-    if (j - 1 >= 0)
-        topple += is_part_stable(grid_copy, grid1, i, j - 1);
-    else
-        topple += 1;
-    if (j + 1 < 3)
-        topple += is_part_stable(grid_copy, grid1, i, j + 1);
-    else
-        topple += 1;
-    return (topple);
+	if (i - 1 >= 0)
+		topple += is_part_stable(grid_copy, grid1, i - 1, j);
+	else
+		topple += 1;
+	if (i + 1 < 3)
+		topple += is_part_stable(grid_copy, grid1, i + 1, j);
+	else
+		topple += 1;
+	if (j - 1 >= 0)
+		topple += is_part_stable(grid_copy, grid1, i, j - 1);
+	else
+		topple += 1;
+	if (j + 1 < 3)
+		topple += is_part_stable(grid_copy, grid1, i, j + 1);
+	else
+		topple += 1;
+	return (topple);
 }
 
 /**
@@ -95,35 +95,35 @@ int topple_sandpile(int grid1[3][3], int grid_copy[3][3], int i, int j)
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j, topple = 0;
-    int grid_copy[3][3];
+	int i, j, topple = 0;
+	int grid_copy[3][3];
 
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++)
-            grid_copy[i][j] = grid1[i][j] + grid2[i][j];
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid_copy[i][j] = grid1[i][j] + grid2[i][j];
 
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 3; j++)
-            grid1[i][j] = grid_copy[i][j];
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] = grid_copy[i][j];
 
-    while (is_stable(grid_copy) == 0)
-    {
-        printf("=\n");
-        print_grid(grid1);
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                topple = 0;
-                if (grid_copy[i][j] > 3)
-                {
-                    topple = topple_sandpile(grid1, grid_copy, i, j);
-                    grid1[i][j] -= topple;
-                }
-            }
-        }
-        for (i = 0; i < 3; i++)
-            for (j = 0; j < 3; j++)
-                grid_copy[i][j] = grid1[i][j];
-    }
+	while (is_stable(grid_copy) == 0)
+	{
+		printf("=\n");
+		print_grid(grid1);
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				topple = 0;
+				if (grid_copy[i][j] > 3)
+				{
+					topple = topple_sandpile(grid1, grid_copy, i, j);
+					grid1[i][j] -= topple;
+				}
+			}
+		}
+		for (i = 0; i < 3; i++)
+			for (j = 0; j < 3; j++)
+				grid_copy[i][j] = grid1[i][j];
+	}
 }
