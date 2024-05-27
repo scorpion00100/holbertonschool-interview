de9b53c35ac7f22ffa4fa62028339501d7037992
scorpion00100 <danbetheliryivuze@gmail.com>
2024-05-27 16:04:03 +0200

    minimum operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+    """
+    Method that calculates the fewest number of operations
+    """
+    
+    if n <= 1:
+        return 0
+
+    minOperations = 0
+
+    while n % 2 == 0:
+        minOperations += 2
+        n /= 2
+
+    i = 3
+    while i * i <= n:
+        while n % i == 0:
+            minOperations += i
+            n /= i
+        i += 2
+    if n > 2:
+        minOperations += n
+
+    return int(minOperations)
