0e36b6f4779bbb5b2a9407bc8b4ac851aa768b0d
scorpion00100 <danbetheliryivuze@gmail.com>
2024-11-26 20:02:24 +0100

    try to fix bug

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -8,14 +8,14 @@
  */
 int max_array(int *array, size_t size)
 {
-    size_t i;
-    int max = array[0];
+	size_t i;
+	int max = array[0];
 
-    for (i = 1; i < size; i++)
-        if (max < array[i])
-            max = array[i];
+	for (i = 1; i < size; i++)
+		if (max < array[i])
+			max = array[i];
 
-    return (max);
+	return (max);
 }
 
 /**
@@ -30,40 +30,40 @@ int max_array(int *array, size_t size)
  */
 void counting_sort(int *array, int size, int exp)
 {
-    int *output, *count;
-    int i;
+	int *output, *count;
+	int i;
 
-    output = malloc(size * sizeof(int));
-    if (!output)
-        return;
+	output = malloc(size * sizeof(int));
+	if (!output)
+		return;
 
-    count = malloc(10 * sizeof(int));
-    if (!count)
-    {
-        free(output);
-        return;
-    }
+	count = malloc(10 * sizeof(int));
+	if (!count)
+	{
+		free(output);
+		return;
+	}
 
-    for (i = 0; i < 10; i++)
-        count[i] = 0;
+	for (i = 0; i < 10; i++)
+		count[i] = 0;
 
-    for (i = 0; i < size; i++)
-        count[(array[i] / exp) % 10]++;
+	for (i = 0; i < size; i++)
+		count[(array[i] / exp) % 10]++;
 
-    for (i = 1; i < 10; i++)
-        count[i] += count[i - 1];
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
 
-    for (i = size - 1; i >= 0; i--)
-    {
-        output[count[(array[i] / exp) % 10] - 1] = array[i];
-        count[(array[i] / exp) % 10]--;
-    }
+	for (i = size - 1; i >= 0; i--)
+	{
+		output[count[(array[i] / exp) % 10] - 1] = array[i];
+		count[(array[i] / exp) % 10]--;
+	}
 
-    for (i = 0; i < size; i++)
-        array[i] = output[i];
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
 
-    free(output);
-    free(count);
+	free(output);
+	free(count);
 }
 
 /**
@@ -71,21 +71,22 @@ void counting_sort(int *array, int size, int exp)
  * order using the Radix sort algorithm.
  *
  * @array: The array to be sorted
- * @size: Number of elements
+ * @size: Number of elements in @array
  * Return: Nothing
  */
 void radix_sort(int *array, size_t size)
 {
-    int max, exp;
+	int max, exp;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    max = max_array(array, size);
+	max = max_array(array, size);
 
-    for (exp = 1; max / exp > 0; exp *= 10)
-    {
-        counting_sort(array, size, exp);
-        print_array(array, size);
-    }
+	for (exp = 1; max / exp > 0; exp *= 10)
+	{
+		counting_sort(array, size, exp);
+		print_array(array, size);
+	}
 }
+
--- radix_sort/sort.h
+++ radix_sort/sort.h
@@ -8,3 +8,4 @@ void print_array(const int *array, size_t size);
 void radix_sort(int *array, size_t size);
 
 #endif
+
