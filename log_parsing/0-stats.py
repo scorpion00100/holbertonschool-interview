74646e188c1aef84488de2496b80455b5ef6f3de
scorpion00100 <danbetheliryivuze@gmail.com>
2025-01-29 00:07:08 +0100

    try to fix bug

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,48 +1,49 @@
 #!/usr/bin/python3
-""" script that reads stdin line by line and computes metrics """
+"""Script that reads stdin line by line and computes metrics."""
 
 if __name__ == '__main__':
 
     import sys
 
     def print_results(statusCodes, fileSize):
-        """ Print statistics """
+        """Print statistics."""
         print("File size: {:d}".format(fileSize))
         for statusCode, times in sorted(statusCodes.items()):
             if times:
                 print("{:s}: {:d}".format(statusCode, times))
 
-    statusCodes = {"200": 0,
-                   "301": 0,
-                   "400": 0,
-                   "401": 0,
-                   "403": 0,
-                   "404": 0,
-                   "405": 0,
-                   "500": 0
-                   }
+    statusCodes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
     fileSize = 0
     n_lines = 0
 
     try:
-        """ Read stdin line by line """
+        # Read stdin line by line
         for line in sys.stdin:
             if n_lines != 0 and n_lines % 10 == 0:
-                """ After every 10 lines, print from the beginning """
+                # After every 10 lines, print from the beginning
                 print_results(statusCodes, fileSize)
             n_lines += 1
             data = line.split()
             try:
-                """ Compute metrics """
+                # Compute metrics
                 statusCode = data[-2]
                 if statusCode in statusCodes:
                     statusCodes[statusCode] += 1
                 fileSize += int(data[-1])
-            except:
-                pass
+            except (IndexError, ValueError):
+                # Handle cases where data[-2] or data[-1] is invalid
+                continue
         print_results(statusCodes, fileSize)
     except KeyboardInterrupt:
-        """ Keyboard interruption, print from the beginning """
+        # Handle keyboard interruption, print from the beginning
         print_results(statusCodes, fileSize)
         raise
-
